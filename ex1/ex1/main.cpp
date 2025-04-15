8cd55318dd927f6a9f117b637f64851d2d58c2d0
ROYS-WIN-PC <royda2@mta.ac.il>
2025-04-15 16:16:57 +0300

    WORKING - added the recursive with simulated stack function

--- ex1/ex1/main.cpp
+++ ex1/ex1/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    bool validArrayInput = false, validArraySizeInput = false, validSumInput = false;
     int i_ArrSize;
     cin >> i_ArrSize;
 
@@ -18,15 +19,27 @@ int main()
     int i_GoalSum;
     cin >> i_GoalSum;
 
-    cout << "Iterative algorithm::" << endl;
-    findPairsIterative(i_Arr, i_ArrSize, i_GoalSum);
+    // validArrayInput = ....  ;
+    // validArraySizeInput = ....  ;
+    // validSumInput = ....  ;
 
-    cout << endl << "Recursive algorithm (recursive version):" << endl;
-    findPairsRecursive(i_Arr, i_ArrSize, i_GoalSum);
+    bool validInput = validArrayInput && validArraySizeInput && validSumInput;
+    if (validInput)
+    {
+        cout << "Iterative algorithm::" << endl;
+        findPairsIterative(i_Arr, i_ArrSize, i_GoalSum);
 
-    cout << endl << "Recursive algorithm (iterative version):" << endl;
-    findPairsStackSimulated(i_Arr, i_ArrSize, i_GoalSum);
+        cout << endl << "Recursive algorithm (recursive version):" << endl;
+        findPairsRecursive(i_Arr, i_ArrSize, i_GoalSum);
 
+        cout << endl << "Recursive algorithm (iterative version):" << endl;
+        findPairsStackSimulated(i_Arr, i_ArrSize, i_GoalSum);
+    }
+    else
+    {
+        cout << "wrong input" << endl;
+    }
+    
     delete[] i_Arr;
     
     return 0;
